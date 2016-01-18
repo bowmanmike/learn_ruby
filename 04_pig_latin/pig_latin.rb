e19fc9c55887fc4389b3f366d8d655caaf1f9a3f
Mike Bowman <mbowman2@gmail.com>
2016-01-18 17:39:29 -0500

    pig_latin mostly working

---
+++ 04_pig_latin/pig_latin.rb
@@ -0,0 +1,72 @@
+def translate(string)
+  vowels = ["a", "e", "i", "o", "u"]
+  string_array = string.split(" ")
+  string_array.map! do |string|
+    if vowels.include?(string[0])
+      string = string + "ay"
+    elsif string[0] == "q" && string[1] == "u"
+      beginning = string.slice!(0, 2)
+      string = string + beginning + "ay"
+    elsif vowels.include?(string[0]) == false && string[1] == "q" && string[2] == "u"
+      beginning = string.slice!(0,3)
+      string = string + beginning + "ay"
+    elsif vowels.include?(string[0] && string[1] && string[2]) == false
+      beginning = string.slice!(0, 3)
+      string = string + beginning + "ay"
+    elsif vowels.include?(string[0] && string[1]) == false
+      beginning = string.slice!(0, 2)
+      string = string + beginning + "ay"
+    elsif  vowels.include?(string[0]) == false
+      beginning = string.slice!(0)
+      string = string + beginning + "ay"
+    end
+  end
+  string = string_array.join(" ")
+
+
+
+### SOLUTION GRAVEYARD ####
+  # end
+
+  # string = string_array.join(" ")
+
+
+
+
+
+  # if string.start_with?("a", "e", "i", "o", "u")
+  #   string = string + "ay"
+  # elsif string.start_with?("qu")
+  #   beginning = string.slice!(0, 2)
+  #   string = string + beginning + "ay"
+  # elsif string[0] != ("a" || "e" || "i" || "o" || "u") && string[1] != ("a" || "e" || "i" || "o" || "u") && string[2] != ("a" || "e" || "i" || "o" || "u")
+  #   beginning = string.slice!(0, 3)
+  #   string = string + beginning + "ay"
+  # elsif string[0] != ("a" || "e" || "i" || "o" || "u") && string[1] != ("a" || "e" || "i" || "o" || "u")
+  #   beginning = string.slice!(0, 2)
+  #   string = string + beginning + "ay"
+  # else
+  #   beginning = string.slice!(0, 1)
+  #   string = string + beginning + "ay"
+  # end
+
+
+
+
+
+  # string = string.downcase.split(" ")
+    # string.join(" ")
+  # if string[0] || string[1] || string[2] != ("a" || "e" || "i" || "o" || "u")
+  #   beginning = string.slice!(0, 3)
+  #   string = string + beginning + "ay"
+  #
+  # elsif string[0] || string[1] != ("a" || "e" || "i" || "o" || "u")
+  #   beginning = string.slice!(0, 2)
+  #   string = string + beginning + "ay"
+  #
+
+  # string = string.split(" ")
+  # string.each do |word|
+  #   word.
+  # end
+end
