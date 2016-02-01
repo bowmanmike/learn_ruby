edbb201b88b7529a3250ba32740dcbc979e99ecd
Mike Bowman <mbowman2@gmail.com>
2016-02-01 16:37:40 -0500

    Use case statement on Changer, returns nil

--- stretch_01_make_change/changer.rb
+++ stretch_01_make_change/changer.rb
@@ -1,7 +1,9 @@
 # require 'pry'
+# Use remainder function ##
 
 class Changer
 
+  @@change = []
 
   @@quarter = 25
   @@dime = 10
@@ -9,133 +11,41 @@ class Changer
   @@penny = 1
 
   def self.make_change(amount)
+    amount = amount.to_i
 
-    change = []
-
-    while amount > 25
+    case amount
+    when amount >= 25
       Changer.subtract_quarters(amount)
-    end
-
-    while amount < 25
+    when amount < 25
       Changer.subtract_dimes(amount)
-    end
-
-    while amount < 10
+    when amount < 10
       Changer.subtract_nickels(amount)
-    end
-
-    while amount < 5
+    when amount < 5
       Changer.subtract_pennies(amount)
     end
   end
 
   def self.subtract_quarters(amount)
-    while amount >= 25
-      change << @@quarter
-      amount -=25
-    end
+    @@change << @@quarter
+    amount -= 25
     amount
   end
 
   def self.subtract_dimes(amount)
-    while amount >= 10
-      change << @@dime
-      amount -=10
-    end
+    @@change << @@dime
+    amount -= 10
     amount
   end
 
   def self.subtract_nickels(amount)
-    while amount >= 10
-      change << @@nickel
-      amount -= 5
-    end
+    @@change << @@nickel
+    amount -= 5
     amount
   end
 
   def self.subtract_pennies(amount)
-    while amount > 0
-      change << @@pennies
-      amount -= 1
-    end
+    @@change << @@penny
+    amount -= 1
     amount
   end
-
-
-  # def self.make_change(amount)
-  #
-  #   change = []
-  #
-  #   # case amount
-  #   # when > 25
-  #   #   subtract_quarter(amount)
-  #   # when
-  #   # end
-  #
-  #   if amount < 5
-  #     while amount > 0 do
-  #       # binding.pry
-  #       change << @@penny
-  #       amount -= 1
-  #     end
-  #
-  #   elsif amount <= 10 && amount >= 5
-  #     change << @@nickel
-  #     amount -= 5
-  #     while amount > 0 do
-  #       change << @@penny
-  #       amount -= 1
-  #     end
-  #
-  #   elsif amount <= 25
-  #     while amount >= 10 do
-  #       change << @@dime
-  #       amount -= 10
-  #       while amount >= 5 && amount < 10 do
-  #         change << @@nickel
-  #         amount -=5
-  #         while amount > 0 do
-  #           change << @@penny
-  #           amount -= 1
-  #         end
-  #       end
-  #     end
-  #
-  #   else
-  #     while amount >= 25 do
-  #       change << @@quarter
-  #       amount -= 25
-  #       while amount >= 10 do
-  #         change << @@dime
-  #         amount -= 10
-  #         while amount >= 5 && amount < 10 do
-  #           change << @@nickel
-  #           amount -=5
-  #           while amount > 0 do
-  #             change << @@penny
-  #             amount -= 1
-  #           end
-  #         end
-  #       end
-  #     end
-  #
-  #
-  #   end
-  #
-  #   return change
-  #
-  #   # if amount == 50
-  #   #   [25, 25]
-  #   # elsif amount == 20
-  #   #   [10, 10]
-  #   # elsif amount == 10
-  #   #   [10]
-  #   # elsif amount == 25
-  #   #   [25]
-  #   # elsif amount == 7
-  #   #   [5, 1, 1]
-  #   # elsif amount == 68
-  #   #   [25, 25, 10, 5, 1, 1, 1]
-  #   # end
-  # end
 end
