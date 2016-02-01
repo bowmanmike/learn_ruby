c4ac4c10aea733538021fca0b5e2256f92d5ec0d
Mike Bowman <mbowman2@gmail.com>
2016-02-01 16:55:08 -0500

    Unsuccessful try on Changer

--- stretch_01_make_change/changer.rb
+++ stretch_01_make_change/changer.rb
@@ -12,40 +12,40 @@ class Changer
 
   def self.make_change(amount)
     amount = amount.to_i
+    @@change = []
 
     case amount
-    when amount >= 25
+    when amount % 25 >= 0
       Changer.subtract_quarters(amount)
-    when amount < 25
+    when amount % 10 >= 0
       Changer.subtract_dimes(amount)
-    when amount < 10
+    when amount % 5 >= 0
       Changer.subtract_nickels(amount)
-    when amount < 5
+    else
       Changer.subtract_pennies(amount)
+
     end
+
+    @@change
   end
 
   def self.subtract_quarters(amount)
     @@change << @@quarter
     amount -= 25
-    amount
   end
 
   def self.subtract_dimes(amount)
     @@change << @@dime
     amount -= 10
-    amount
   end
 
   def self.subtract_nickels(amount)
     @@change << @@nickel
     amount -= 5
-    amount
   end
 
   def self.subtract_pennies(amount)
     @@change << @@penny
     amount -= 1
-    amount
   end
 end
