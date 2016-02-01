bac66c38ce11e831e675b4047fa92f214cf493f3
Mike Bowman <mbowman2@gmail.com>
2016-02-01 18:19:28 -0500

    Solve Changer.rb

--- stretch_01_make_change/changer.rb
+++ stretch_01_make_change/changer.rb
@@ -5,47 +5,17 @@ class Changer
 
   @@change = []
 
-  @@quarter = 25
-  @@dime = 10
-  @@nickel = 5
-  @@penny = 1
-
   def self.make_change(amount)
-    amount = amount.to_i
     @@change = []
 
-    case amount
-    when amount % 25 >= 0
-      Changer.subtract_quarters(amount)
-    when amount % 10 >= 0
-      Changer.subtract_dimes(amount)
-    when amount % 5 >= 0
-      Changer.subtract_nickels(amount)
-    else
-      Changer.subtract_pennies(amount)
-
-    end
+    (amount / 25).times { @@change << 25 }
+    amount = amount % 25
+    (amount / 10).times { @@change << 10 }
+    amount = amount % 10
+    (amount / 5).times { @@change << 5 }
+    amount = amount % 5
+    amount.times { @@change << 1 }
 
     @@change
-  end
-
-  def self.subtract_quarters(amount)
-    @@change << @@quarter
-    amount -= 25
-  end
-
-  def self.subtract_dimes(amount)
-    @@change << @@dime
-    amount -= 10
-  end
-
-  def self.subtract_nickels(amount)
-    @@change << @@nickel
-    amount -= 5
-  end
-
-  def self.subtract_pennies(amount)
-    @@change << @@penny
-    amount -= 1
   end
 end
